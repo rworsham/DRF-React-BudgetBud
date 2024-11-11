b3551f31db768fc60682cd1bd29879b972bf48ca
rworsham <robertworsham01@gmail.com>
2024-11-10 19:06:51 -0500

    Corrected missing comma in settings

--- budget_bud/budget_bud/settings.py
+++ budget_bud/budget_bud/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'budget_bud_api.apps.BudgetBudApiConfig',
     'rest_framework',
-    'rest_framework_simplejwt'
+    'rest_framework_simplejwt',
     'corsheaders',
 ]
 
